de8d1e5e06612813f613b94fce6c3f4686e229fe
Zirlis <Zirlis@yandex.ru>
2020-12-03 22:23:41 +0700

    Final work

--- Artemkin.HomeWork.26.11/Form1.Designer.cs
+++ Artemkin.HomeWork.26.11/Form1.Designer.cs
@@ -4,14 +4,14 @@ namespace Artemkin.HomeWork._26._11
     partial class Form1
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,21 +21,193 @@ namespace Artemkin.HomeWork._26._11
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.miNew = new System.Windows.Forms.ToolStripButton();
+            this.miOpen = new System.Windows.Forms.ToolStripButton();
+            this.miSave = new System.Windows.Forms.ToolStripButton();
+            this.miExit = new System.Windows.Forms.ToolStripButton();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSaveQuest = new System.Windows.Forms.Button();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miNew,
+            this.miOpen,
+            this.miSave,
+            this.miExit});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(800, 31);
+            this.toolStrip.TabIndex = 0;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // miNew
+            // 
+            this.miNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.miNew.Image = ((System.Drawing.Image)(resources.GetObject("miNew.Image")));
+            this.miNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.miNew.Name = "miNew";
+            this.miNew.Size = new System.Drawing.Size(28, 28);
+            this.miNew.Text = "Создать";
+            this.miNew.Click += new System.EventHandler(this.miNew_Click);
+            // 
+            // miOpen
+            // 
+            this.miOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.miOpen.Image = ((System.Drawing.Image)(resources.GetObject("miOpen.Image")));
+            this.miOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.miOpen.Name = "miOpen";
+            this.miOpen.Size = new System.Drawing.Size(28, 28);
+            this.miOpen.Text = "Открыть";
+            this.miOpen.Click += new System.EventHandler(this.miOpen_Click);
+            // 
+            // miSave
+            // 
+            this.miSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.miSave.Image = ((System.Drawing.Image)(resources.GetObject("miSave.Image")));
+            this.miSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.miSave.Name = "miSave";
+            this.miSave.Size = new System.Drawing.Size(28, 28);
+            this.miSave.Text = "Сохранить";
+            this.miSave.Click += new System.EventHandler(this.miSave_Click);
+            // 
+            // miExit
+            // 
+            this.miExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.miExit.Image = ((System.Drawing.Image)(resources.GetObject("miExit.Image")));
+            this.miExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(28, 28);
+            this.miExit.Text = "Выйти";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(552, 418);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(120, 20);
+            this.nudNumber.TabIndex = 1;
+            this.nudNumber.ValueChanged += new System.EventHandler(this.nudNumber_ValueChanged);
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.mainTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainTextBox.Location = new System.Drawing.Point(12, 34);
+            this.mainTextBox.Multiline = true;
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.Size = new System.Drawing.Size(776, 379);
+            this.mainTextBox.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 415);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDelete.Location = new System.Drawing.Point(194, 415);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSaveQuest
+            // 
+            this.btnSaveQuest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSaveQuest.Location = new System.Drawing.Point(275, 415);
+            this.btnSaveQuest.Name = "btnSaveQuest";
+            this.btnSaveQuest.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveQuest.TabIndex = 6;
+            this.btnSaveQuest.Text = "Сохранить";
+            this.btnSaveQuest.UseVisualStyleBackColor = true;
+            this.btnSaveQuest.Click += new System.EventHandler(this.btnSaveQuest_Click);
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(678, 421);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(64, 17);
+            this.checkBox.TabIndex = 7;
+            this.checkBox.Text = "Правда";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(768, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 20);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.btnSaveQuest);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.mainTextBox);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.toolStrip);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton miNew;
+        private System.Windows.Forms.ToolStripButton miOpen;
+        private System.Windows.Forms.ToolStripButton miSave;
+        private System.Windows.Forms.ToolStripButton miExit;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.TextBox mainTextBox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSaveQuest;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Button button1;
     }
 }
-
